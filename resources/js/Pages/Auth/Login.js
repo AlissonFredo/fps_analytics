632fa3f86ae8247c575718b595cd5e0b9bdc7ae1
Douglas Alisson <douglas.alisson@ibridge.com.br>
2022-05-06 09:54:35 -0300

    starting login page tweak

--- resources/js/Pages/Auth/Login.js
+++ resources/js/Pages/Auth/Login.js
@@ -31,64 +31,156 @@ export default function Login({ status, canResetPassword }) {
     };
 
     return (
-        <Guest>
-            <Head title="Log in" />
+        // <Guest>
+        //     <Head title="Log in" />
 
-            {status && <div className="mb-4 font-medium text-sm text-green-600">{status}</div>}
+        //     {status && <div className="mb-4 font-medium text-sm text-green-600">{status}</div>}
 
-            <ValidationErrors errors={errors} />
+        //     <ValidationErrors errors={errors} />
 
-            <form onSubmit={submit}>
-                <div>
-                    <Label forInput="email" value="Email" />
+        //     <form onSubmit={submit}>
+        //         <div>
+        //             <Label forInput="email" value="Email" />
 
-                    <Input
-                        type="text"
-                        name="email"
-                        value={data.email}
-                        className="mt-1 block w-full"
-                        autoComplete="username"
-                        isFocused={true}
-                        handleChange={onHandleChange}
-                    />
+        //             <Input
+        //                 type="text"
+        //                 name="email"
+        //                 value={data.email}
+        //                 className="mt-1 block w-full"
+        //                 autoComplete="username"
+        //                 isFocused={true}
+        //                 handleChange={onHandleChange}
+        //             />
+        //         </div>
+
+        //         <div className="mt-4">
+        //             <Label forInput="password" value="Password" />
+
+        //             <Input
+        //                 type="password"
+        //                 name="password"
+        //                 value={data.password}
+        //                 className="mt-1 block w-full"
+        //                 autoComplete="current-password"
+        //                 handleChange={onHandleChange}
+        //             />
+        //         </div>
+
+        //         <div className="block mt-4">
+        //             <label className="flex items-center">
+        //                 <Checkbox name="remember" value={data.remember} handleChange={onHandleChange} />
+
+        //                 <span className="ml-2 text-sm text-gray-600">Remember me</span>
+        //             </label>
+        //         </div>
+
+        //         <div className="flex items-center justify-end mt-4">
+        //             {canResetPassword && (
+        //                 <Link
+        //                     href={route('password.request')}
+        //                     className="underline text-sm text-gray-600 hover:text-gray-900"
+        //                 >
+        //                     Forgot your password?
+        //                 </Link>
+        //             )}
+
+        //             <Button className="ml-4" processing={processing}>
+        //                 Log in
+        //             </Button>
+        //         </div>
+        //     </form>
+        // </Guest>
+
+
+        <div className='login-page'>
+
+
+            <div className='card w-50 p-3'>
+                <div className='card-body'>
+                    <form action="../../index3.html" method="post">
+                        <div className="input-group mb-3">
+                            <input type="email" className="form-control" placeholder="Email" />
+                            <div className="input-group-append">
+                                <div className="input-group-text">
+                                    <span className="fas fa-envelope"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="input-group mb-3">
+                            <input type="password" className="form-control" placeholder="Password" />
+                            <div className="input-group-append">
+                                <div className="input-group-text">
+                                    <span className="fas fa-lock"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-12">
+                                <div className="icheck-primary">
+                                    <input type="checkbox" id="remember" />
+                                    <label for="remember">
+                                        Remember Me
+                                    </label>
+                                </div>
+                            </div>
+
+                            <div className="col-12 text-right">
+                                <button type="submit" className="btn btn-primary">Sign In</button>
+                            </div>
+
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+            {/* <div className="login-box">
+                <div className="login-logo">
+                    <a href="../../index2.html">Coach Analytics</a>
                 </div>
 
-                <div className="mt-4">
-                    <Label forInput="password" value="Password" />
+                <div className="w-75 card">
+                    <div className="card-body login-card-body">
+                        <form action="../../index3.html" method="post">
+                            <div className="input-group mb-3">
+                                <input type="email" className="form-control" placeholder="Email" />
+                                    <div className="input-group-append">
+                                        <div className="input-group-text">
+                                            <span className="fas fa-envelope"></span>
+                                        </div>
+                                    </div>
+                            </div>
+                            <div className="input-group mb-3">
+                                <input type="password" className="form-control" placeholder="Password" />
+                                    <div className="input-group-append">
+                                        <div className="input-group-text">
+                                            <span className="fas fa-lock"></span>
+                                        </div>
+                                    </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-8">
+                                    <div className="icheck-primary">
+                                        <input type="checkbox" id="remember"/>
+                                            <label for="remember">
+                                                Remember Me
+                                            </label>
+                                    </div>
+                                </div>
 
-                    <Input
-                        type="password"
-                        name="password"
-                        value={data.password}
-                        className="mt-1 block w-full"
-                        autoComplete="current-password"
-                        handleChange={onHandleChange}
-                    />
+                                <div className="col-4">
+                                    <button type="submit" className="btn btn-primary btn-block">Sign In</button>
+                                </div>
+
+                            </div>
+                        </form>
+                    </div>
                 </div>
+            </div> */}
 
-                <div className="block mt-4">
-                    <label className="flex items-center">
-                        <Checkbox name="remember" value={data.remember} handleChange={onHandleChange} />
 
-                        <span className="ml-2 text-sm text-gray-600">Remember me</span>
-                    </label>
-                </div>
+        </div>
 
-                <div className="flex items-center justify-end mt-4">
-                    {canResetPassword && (
-                        <Link
-                            href={route('password.request')}
-                            className="underline text-sm text-gray-600 hover:text-gray-900"
-                        >
-                            Forgot your password?
-                        </Link>
-                    )}
 
-                    <Button className="ml-4" processing={processing}>
-                        Log in
-                    </Button>
-                </div>
-            </form>
-        </Guest>
+
     );
 }
